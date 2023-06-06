37d442032849b87550a6e6c4c5b3696d0b1eb1b4
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-06-06 20:12:23 +0800

    Initial Commit

---
+++ StarterChatbot/StarterChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterChatbotApp.swift
+//  StarterChatbot
+//
+//  Created by Atul Parida on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct StarterChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
